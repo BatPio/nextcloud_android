8e581ba92d6a9b2ffa6e8d8b76fc59a48903d16e
batpio <prbator@gmail.com>
2022-12-10 01:11:03 +0100

    Delayed synchronization

--- app/src/main/java/com/owncloud/android/datamodel/SyncDelay.java
+++ app/src/main/java/com/owncloud/android/datamodel/SyncDelay.java
@@ -2,35 +2,33 @@ package com.owncloud.android.datamodel;
 
 import com.google.gson.GsonBuilder;
 
-import java.util.Date;
-
 public class SyncDelay {
     private boolean enabled;
-    private RangeType type;
+    private TimestampType type;
     private long olderThan;
 
-    public SyncDelay(boolean enabled, RangeType type, long olderThan) {
+    public SyncDelay(boolean enabled, TimestampType type, long olderThan) {
         this.enabled = enabled;
         this.type = type;
         this.olderThan = olderThan;
     }
 
     public boolean satisfies(long timestamp) {
-        return toRangeEdge(type, olderThan).getTime() > timestamp;
+        return toAbsoluteTimestamp(type, olderThan) > timestamp;
     }
 
-    private Date toRangeEdge(RangeType dateType, long timestamp) {
-        if(RangeType.RELATIVE == dateType) {
-            return new Date(System.currentTimeMillis() - timestamp);
+    private long toAbsoluteTimestamp(TimestampType dateType, long timestamp) {
+        if (TimestampType.RELATIVE == dateType) {
+            return System.currentTimeMillis() - timestamp;
         }
-        return new Date(timestamp);
+        return timestamp;
     }
 
     public static SyncDelay deserialize(String config) {
         String[] parts = config.split("|");
         return new SyncDelay(
             Boolean.valueOf(parts[0]),
-            RangeType.valueOf(parts[1]),
+            TimestampType.valueOf(parts[1]),
             Long.valueOf(parts[2])
         );
     }
@@ -47,7 +45,7 @@ public class SyncDelay {
         return new GsonBuilder().create().toJson(this, SyncDelay.class);
     }
 
-    private enum RangeType {
+    private enum TimestampType {
         RELATIVE, ABSOLUTE
     }
 
